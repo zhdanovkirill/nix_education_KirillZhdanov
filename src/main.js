7453f64b6ba44f59f6a6b71337878cce590d9e8b
zhdanovkirill <k.zhdanov@newlinetechnologies.net>
2022-08-25 18:56:28 +0300

    HW9

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import {items as allDevice} from './app/helpers/fake-backend.js';
 import {getRandomInt} from "./app/helpers/utils.js";
+import {showDetails} from "./app/components/device-details/device-details.js";
+import {FilterComponent} from "./app/components/filter/filterComponent.js";
+import {Filter} from "./app/helpers/filter.js";
 
 const itemsContainer = document.getElementById("items");
 const card = document.getElementById("card");
+
+
+let filter = new Filter();
+
+let filterComponent = new FilterComponent(filter, allDevice)
+filterComponent.filterActionHandler(renderDevices);
+
 function renderDevices() {
-    let filterObjects = allDevice
+    let filterObjects = filter.filtration(allDevice)
 
     itemsContainer.innerHTML = "";
     filterObjects.map(item => {
@@ -12,6 +22,7 @@ function renderDevices() {
         let newCard = document.createElement("div");
         newCard.innerHTML = card.innerHTML;
         newCard.classList.add("card")
+        newCard.onclick = (event) => showDetails(event, item);
 
         let cardImage = newCard.getElementsByClassName("card-image");
         cardImage[0].src = `resources/${item.imgUrl}`;
@@ -46,10 +57,17 @@ function renderDevices() {
         if (item.orderInfo.inStock === 0) {
             btnAdd.disabled = true;
         }
+        btnAdd.addEventListener('click', (event) => addToCart(event, item));
 
         price.textContent = ` ${item.price} `;
         itemsContainer.appendChild(newCard);
     })
+}
+
+function addToCart(event, device) {
+    console.log('addToCart', device, event)
+    event.stopPropagation();
+    //  console.log(allDevice.find(item => item.id===deviceId))
 }
 
 renderDevices(allDevice);
