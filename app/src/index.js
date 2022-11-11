64a4307375194d53aaa2bfcadb391fc7a03b1f35
zhdanovkirill <59727742+zhdanovkirill@users.noreply.github.com>
2022-11-11 21:53:57 +0200

    test
    added test endpoint

--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,6 @@ const cors = require('cors')
 const http = require('http');
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Hi</h1>`);
-});
-
 app.use(cors())
 
 const messages = [
@@ -16,10 +12,19 @@ const messages = [
 ]
 
 
+app.get('/', (req, res) => {
+    res.send(`<h1>Hi</h1>`);
+});
+
 app.get('/messages', (req, res) => {
     res.contentType('application/json')
     res.send(JSON.stringify(messages));
 });
+
+app.get('/test', (req, res) => {
+    res.send(`<h1>Hi</h1><span> my first deploying</span>`);
+});
+
 
 server.listen((process.env.PORT || 3001), function(){
     console.log('Listening on http://localhost:3001');
